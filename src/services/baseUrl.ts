aa0ea0cc12ee6f3e4935b7a1c899af39592eb33e
Charles Caleb <charlesofficial9@gmail.com>
2025-02-21 18:18:50 +0100

    update

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,6 +1,6 @@
 export const baseUrl = () => {
-  // const url =
-  //   "https://stafff-portal-backend-production.up.railway.app/api/v1/casfod";
-  const url = "http://127.0.0.1:4002/api/v1/casfod";
+  const url =
+    "https://stafff-portal-backend-production.up.railway.app/api/v1/casfod";
+  // const url = "http://127.0.0.1:4002/api/v1/casfod";
   return url;
 };
