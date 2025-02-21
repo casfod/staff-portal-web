7fb309acb61958b6d22794dfff48275c3b3e3003
Charles Caleb <charlesofficial9@gmail.com>
2025-02-21 10:44:29 +0100

    update

--- src/pages/UserManagement.tsx
+++ src/pages/UserManagement.tsx
@@ -7,12 +7,18 @@ import Modal from "../ui/Modal";
 import Swal from "sweetalert2";
 import UserCard from "../features/user/UserCard";
 import Spinner from "../ui/Spinner";
+import { BiSearch } from "react-icons/bi";
+import { useEffect, useState } from "react";
+import { UserType } from "../interfaces";
 
 export function UserManagement() {
   const localStorageUserX = localStorageUser();
-  const { data } = useUsers();
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredUsers, setFilteredUsers] = useState<UserType[]>([]);
+
+  const { data, isLoading } = useUsers();
   const { deleteUser } = useDeleteUser();
-  const users = data?.data || [];
+  const users = data?.data;
 
   const handleDelete = (id: string) => {
     Swal.fire({
@@ -34,8 +40,22 @@ export function UserManagement() {
     });
   };
 
+  useEffect(() => {
+    if (users) {
+      const term = searchTerm.toLowerCase();
+      const filtered = users.filter(
+        (user: UserType) =>
+          user.first_name.toLowerCase().includes(term) ||
+          user.last_name.toLowerCase().includes(term) ||
+          user.email.toLowerCase().includes(term) ||
+          user.role.toLowerCase().includes(term)
+      );
+      setFilteredUsers(filtered);
+    }
+  }, [searchTerm, users]);
+
   return (
-    <div className="space-y-6">
+    <div className="flex flex-col space-y-6">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-semibold text-gray-800">
           User Management
@@ -52,6 +72,18 @@ export function UserManagement() {
         )}
       </div>
 
+      <div className="flex items-center w-full max-w-[275px] h-9 bg-white border border-gray-300 rounded-lg shadow-sm focus-within:border-gray-400 transition">
+        <span className="p-2 text-gray-500">
+          <BiSearch className="w-5 h-5" />
+        </span>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full h-full px-2 text-gray-700 placeholder-gray-400 rounded-lg focus:outline-none focus:ring-0"
+          placeholder="Search by Name, Email, or Role"
+        />
+      </div>
       <div className="bg-white shadow-sm rounded-lg overflow-hidden">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
@@ -77,7 +109,7 @@ export function UserManagement() {
           </thead>
 
           <tbody className="bg-white divide-y divide-gray-200">
-            {users?.map((user) => (
+            {filteredUsers?.map((user) => (
               <tr key={user.id}>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
                   <p className="inline-flex gap-1 text-gray-800 font-medium">
@@ -119,10 +151,13 @@ export function UserManagement() {
           </tbody>
         </table>
 
-        {users.length === 0 && (
+        {isLoading && (
           <div className="bg-gray-50 flex items-center justify-center w-full h-64">
             <Spinner />
           </div>
+        )}
+        {filteredUsers.length === 0 && (
+          <p className="text-2xl text-center text-gray-500">No result</p>
         )}
       </div>
 
@@ -131,7 +166,7 @@ export function UserManagement() {
         <Modal.Window name="addUser">
           <AddUserForm />
         </Modal.Window>
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <Modal.Window key={user.id} name={`userCog-${user.id}`}>
             <UserCard user={user} />
           </Modal.Window>
