859bec9a013f59f8c4739ac79e5d8ec168047ad2
Charles Caleb <charlesofficial9@gmail.com>
2025-02-21 11:49:02 +0100

    update

--- src/features/user/UserCard.tsx
+++ src/features/user/UserCard.tsx
@@ -80,7 +80,7 @@ const UserCard: React.FC<UserCardProps> = ({ user }) => {
           <p>
             <span className="font-semibold">Status:</span>{" "}
             <span
-              className={`text-center px-2 py-1 rounded-full text-xs ${
+              className={`inline-block text-center px-2 py-1 rounded-full text-xs ${
                 user.isDeleted
                   ? "bg-red-100 text-red-700"
                   : "bg-green-100 text-green-700"
