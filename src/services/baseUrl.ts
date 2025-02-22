b3a611ce0fd62fdba68cfdeb36fd5bc1108dc346
Charles Caleb <charlesofficial9@gmail.com>
2025-02-22 17:23:11 +0100

    update

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,3 +1,9 @@
+// export const baseUrl = () => {
+//   return import.meta.env.VITE_API_BASE_URL;
+// };
+
 export const baseUrl = () => {
-  return import.meta.env.VITE_API_BASE_URL;
+  const url =
+    "https://stafff-portal-backend-production.up.railway.app/api/v1/casfod";
+  return url;
 };
