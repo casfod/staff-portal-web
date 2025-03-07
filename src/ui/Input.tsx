2e23141070df5260aa688e80232aee8aa221783d
Charles Caleb <charlesofficial9@gmail.com>
2025-03-07 14:50:58 +0100

    update

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -5,7 +5,8 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ inputSize = "default", ...props }) => {
-  const baseClasses = "border-2 border-gray-300 bg-white rounded-lg p-1";
+  const baseClasses =
+    "text-gray-600 text-[16px]  border-2 border-gray-300 bg-white rounded-lg px-2 py-1";
   const wideClasses = inputSize ? `w-[${inputSize}]` : "w-full";
 
   return <input className={`${baseClasses} ${wideClasses}`} {...props} />;
