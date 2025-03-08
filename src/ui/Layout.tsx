781d01a1ee4c0b84f010e158f57c1b7351ae5930
Charles Caleb <charlesofficial9@gmail.com>
2025-03-08 04:53:24 +0100

    update

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -11,7 +11,10 @@ export function Layout() {
       <Header />
       <div className="flex text-gray-300 overflow-y-hidden">
         {/* <Navigation onNavigate={setCurrentPage} currentPage={currentPage}/> */}
-        <Navigation />
+        <div className="">
+          <Navigation />
+        </div>
+
         <main className="flex-1 mx-auto px-4 md:px-6 pt-4 md:pt-6 pb-8 md:pb-24 h-screen overflow-y-scroll">
           {<Outlet />}
         </main>
--- src/ui/Navigation.tsx
+++ src/ui/Navigation.tsx
@@ -56,7 +56,7 @@ const Navigation: React.FC = () => {
   ];
 
   return (
-    <div className="border hidden lg:flex flex-col items-center h-screen">
+    <div className="border-r hidden lg:flex flex-col items-center  h-full">
       <nav>
         <ul className="flex flex-col items-center w-60 shadow-sm gap-2 px-6 pt-6">
           {navigation.map((item) => (
@@ -94,7 +94,7 @@ const Navigation: React.FC = () => {
       </nav>
 
       <button
-        className="px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-buttonColor hover:bg-buttonColorHover mt-[190px]"
+        className="px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-buttonColor hover:bg-buttonColorHover mt-[80%] "
         onClick={handleLogout}
       >
         {isPending ? (
