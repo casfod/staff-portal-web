c29e04ccea0538bcb9213d511b1ec93002c6626f
Charles Caleb <charlesofficial9@gmail.com>
2025-04-16 18:50:12 +0100

    update

--- src/ui/MobileNavigation.tsx
+++ src/ui/MobileNavigation.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 
 const MobileNavigation = () => {
   return (
-    <div className="fixed flex flex-col items-center top-[58px] left-0 right-0 z-40 bg-white shadow-lg min-h-screen w-fit pt-3">
+    <div className="fixed flex flex-col items-center top-[62px] left-0 right-0 z-40 bg-white shadow-lg min-h-screen w-fit pt-3">
       <div className="flex justify-center border border-gray-200 w-[190px] px-3 py-1 rounded-md shadow-md">
         <img src={logo} alt="CASFOD" className="self-center w-full h-15" />
       </div>
