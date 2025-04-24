3be66c47a6bfee2565d0da6b46f1e151ed93d9ca
Charles Caleb <charlesofficial9@gmail.com>
2025-04-24 16:07:42 +0100

    update

--- src/features/authentication/authHooks/useResetPassword.ts
+++ src/features/authentication/authHooks/useResetPassword.ts
@@ -1,17 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import { resetPassword as resetPasswordApi } from "../../../services/apiAuth.ts";
-import { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { PasswordResetTypes } from "../../../interfaces.ts";
-
-interface ErrorResponse {
-  message: string;
-}
-
-interface LoginError extends AxiosError {
-  response?: AxiosResponse<ErrorResponse>;
-}
 
 export function useResetPassword(token: string) {
   const navigate = useNavigate();
@@ -25,22 +16,20 @@ export function useResetPassword(token: string) {
       resetPasswordApi(token, { password, passwordConfirm }),
 
     onSuccess: (data) => {
-      if (data.status === 200) {
+      if (data.status === "success") {
+        // console.log(data.data.user);
         toast.success("Password reset successful");
 
-        navigate("/auth", { replace: true });
+        navigate("/login", { replace: true });
       } else {
         toast.error(`${data.message}`);
-
         console.error("resetPassword Error:", data.message);
       }
     },
 
-    onError: (err: LoginError) => {
-      toast.error(`${err.response?.data.message}` || "An error occurred");
-
-      const error = err.response?.data.message;
-      console.error("resetPassword Error:", error);
+    onError: (err) => {
+      toast.error("Network or server error");
+      console.log("ERROR", err);
     },
   });
 
