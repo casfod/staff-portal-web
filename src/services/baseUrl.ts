dcde79c044580a05b5d610ac3a633dab18970150
Charles Caleb <charlesofficial9@gmail.com>
2025-04-24 21:02:09 +0100

    update

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,9 +1,9 @@
-export const baseUrl = () => {
-  return import.meta.env.VITE_API_BASE_URL;
-};
-
 // export const baseUrl = () => {
-//   const url =
-//     "https://stafff-portal-backend-production.up.railway.app/api/v1/casfod";
-//   return url;
+//   return import.meta.env.VITE_API_BASE_URL;
 // };
+
+export const baseUrl = () => {
+  const url =
+    "https://stafff-portal-backend-production.up.railway.app/api/v1/casfod";
+  return url;
+};
