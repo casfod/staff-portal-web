5bfa62e304a611a658550991b8baec2b41116588
Charles Caleb <charlesofficial9@gmail.com>
2025-05-22 15:07:22 +0100

    update

--- src/ui/TableData.tsx
+++ src/ui/TableData.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TableData = ({ children }: { children: React.ReactNode }) => {
   return (
-    <td className="px-3 py-1.5 md:px-6 md:py-2 whitespace-nowrap  text-xs 2xl:text-sm text-gray-600 uppercase">
+    <td className="px-3 py-1.5 md:px-6 md:py-2 whitespace-nowrap  text-xs 2xl:text-sm text-gray-600 uppercase truncate">
       {children}
     </td>
   );
