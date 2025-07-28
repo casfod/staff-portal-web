a81f233576c855473f019689de29ab7d87d25210
Charles Caleb <charlesofficial9@gmail.com>
2025-07-28 12:23:03 +0100

    update

--- src/pages/ConceptNotes.tsx
+++ src/pages/ConceptNotes.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { MaintenanceBanner } from "../ui/MaintenanceBanner";
 
 export function ConceptNotes() {
+  const isUnderMaintenance = true;
   return (
     <div className="space-y-2 md:space-y-4 xl:space-y-6">
-      {" "}
-      <Outlet />
+      {isUnderMaintenance ? (
+        <MaintenanceBanner
+          title="Concept Notes Under Maintenance"
+          message="We're addressing a purchase request error."
+          expectedCompletion="Will Be Back Very soon "
+        />
+      ) : (
+        <Outlet />
+      )}
     </div>
   );
 }
