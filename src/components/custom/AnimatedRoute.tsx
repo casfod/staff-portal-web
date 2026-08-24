b26901a357641ccc1441333dfc528643fb3cb1fd
thedrizzyway <christopheriliyas@gmail.com>
2026-08-24 02:44:50 +0100

    fix framer build error

--- src/components/custom/AnimatedRoute.tsx
+++ src/components/custom/AnimatedRoute.tsx
@@ -1,12 +1,16 @@
 // src/components/AnimatedRoute.tsx
 import { AnimatePresence, motion } from 'framer-motion';
+import type { Variants } from 'framer-motion';
 
-const pageVariants = {
+const pageVariants: Variants = {
   initial: { opacity: 0, y: -20 },
   animate: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.5, ease: [0.6, -0.05, 0.01, 0.99] },
+    transition: {
+      duration: 0.5,
+      ease: [0.6, -0.05, 0.01, 0.99] as [number, number, number, number],
+    },
   },
   exit: {
     opacity: 0,
